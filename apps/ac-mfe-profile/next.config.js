384a74bc7d5b87725062738f0f1d9f7b7e04b67d
amaralc <amaral.calil@gmail.com>
2022-10-13 21:29:59 -0300

    fix: remove remotes from profile

--- apps/ac-mfe-profile/next.config.js
+++ apps/ac-mfe-profile/next.config.js
@@ -1,14 +1,6 @@
 //@ts-check
 
 const NextFederationPlugin = require('@module-federation/nextjs-mf');
-// this enables you to use import() and the webpack parser
-// loading remotes on demand, not ideal for SSR
-const remotes = isServer => {
-  const location = isServer ? 'ssr' : 'chunks';
-  return {
-    profile: `profile@${process.env.NEXT_PUBLIC_REMOTE_URL_PROFILE}/_next/static/${location}/remoteEntry.js`,
-  };
-};
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withNx } = require('@nrwl/next/plugins/with-nx');
@@ -31,7 +23,7 @@ const nextConfig = {
         exposes: {
           './HeroSection': './components/HeroSection/index.tsx',
         },
-        remotes: remotes(options.isServer),
+        remotes: {},
         shared: {}
       }),
     );
