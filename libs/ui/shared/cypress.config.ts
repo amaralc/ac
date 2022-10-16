6b6076a0d64de4a8236092cf21d68cad700b7b7f
amaralc <amaral.calil@gmail.com>
2022-10-15 21:58:10 -0300

    config: adjust cypress component tests

---
+++ libs/ui/shared/cypress.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'cypress';
+
+/**
+ * Use Next.js alpha framework
+ *
+ * @see https://larsmagnus.co/blog/component-testing-with-cypress-in-next-js
+ */
+
+export default defineConfig({
+  component: {
+    devServer: {
+      framework: 'next',
+      bundler: 'webpack',
+    },
+  },
+});
