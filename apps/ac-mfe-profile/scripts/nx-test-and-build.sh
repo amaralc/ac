30c30fbaf7be1794f20d91ceb2cd1ecac2ca8ea1
amaralc <amaral.calil@gmail.com>
2022-10-19 00:47:47 -0300

    fix: simplify scripts

--- apps/ac-mfe-profile/scripts/nx-test-and-build.sh
+++ apps/ac-mfe-profile/scripts/nx-test-and-build.sh
@@ -1,24 +1,26 @@
-runContinuousIntegrationPipeline(){
-  echo '>> Running component tests for affected project...'
-  yarn nx component-test ui --parallel
+# runContinuousIntegrationPipeline(){
+#   echo '>> Running component tests for affected project...'
+#   yarn nx component-test ui --parallel
 
-  if [ $? -eq 0 ]
-  then
-    echo '>> Evaluating exit code:' $?
-    echo '>> ✔ Tests have passed!'
-    echo '>> Initializing build...'
-    npx nx build ac-mfe-profile --prod
-  else
-    echo '>> 🛑 Build cancelled since tests have failed.'
-  fi
-}
+#   if [ $? -eq 0 ]
+#   then
+#     echo '>> Evaluating exit code:' $?
+#     echo '>> ✔ Tests have passed!'
+#     echo '>> Initializing build...'
+#     npx nx build ac-mfe-profile --prod
+#   else
+#     echo '>> 🛑 Build cancelled since tests have failed.'
+#   fi
+# }
 
-CURRENT_BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
-echo 'Current branch:' $CURRENT_BRANCH
+# CURRENT_BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+# echo 'Current branch:' $CURRENT_BRANCH
 
-if [ $CURRENT_BRANCH = production ]; then
-  apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
-  runContinuousIntegrationPipeline
-else
-  npx nx build ac-mfe-profile --prod
-fi
+# if [ $CURRENT_BRANCH = production ]; then
+#   apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+#   runContinuousIntegrationPipeline
+# else
+#   npx nx build ac-mfe-profile --prod
+# fi
+
+npx nx build ac-mfe-profile --prod
